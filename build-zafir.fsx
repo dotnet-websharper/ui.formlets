cddfac37fa0f4ef88eb036c9cff04d527cf4b3dc
István Gansperger <gansperger@gmail.com>
2015-12-14 12:59:25 +0100

    Add zafir build

---
+++ build-zafir.fsx
@@ -0,0 +1,56 @@
+#load "tools/includes.fsx"
+
+open IntelliFactory.Build
+
+let bt =
+    BuildTool().PackageId("Zafir.UI.Next.Formlets")
+        .VersionFrom("Zafir", "alpha")
+        .WithFSharpVersion(FSharpVersion.FSharp30)
+        .WithFramework(fun fw -> fw.Net40)
+
+let main =
+    bt.Zafir.Library("WebSharper.UI.Next.Formlets")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.UI.Next").ForceFoundVersion().Reference()
+            ])
+        .Embed(
+            [
+                "styles/Formlet.css"
+                "images/ActionAdd.png"
+                "images/ActionCheck.png"
+                "images/ActionDelete.png"
+                "images/ErrorIcon.png"
+                "images/InfoIcon.png"
+            ])
+
+let test =
+    bt.Zafir.SiteletWebsite("WebSharper.UI.Next.Formlets.Tests")
+        .SourcesFromProject()
+        .References(fun r ->
+            [
+                r.NuGet("Zafir.UI.Next").Reference()
+                r.Project(main)
+            ])
+
+bt.Solution [
+
+    main
+    test
+
+    bt.NuGet.CreatePackage()
+        .Description("Provides a framework to build reactive forms in Zafir.")
+        .ProjectUrl("http://github.com/intellifactory/websharper.ui.next.formlets")
+        .Configure(fun c ->
+            {
+                c with
+                    Authors = ["IntelliFactory"]
+                    Title = Some "Zafir.UI.Next.Formlets"
+                    LicenseUrl = Some "http://github.com/intellifactory/websharper.ui.next.formlets/blob/master/LICENSE.md"
+                    RequiresLicenseAcceptance = true
+            })
+        .Add(main)
+
+]
+|> bt.Dispatch
