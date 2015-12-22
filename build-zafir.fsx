34c51418cf27551e88f373b862e345354cefa71f
István Gansperger <gansperger@gmail.com>
2015-12-22 11:55:50 +0100

    Add latest flag

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -13,7 +13,7 @@ let main =
         .SourcesFromProject()
         .References(fun r ->
             [
-                r.NuGet("Zafir.UI.Next").ForceFoundVersion().Reference()
+                r.NuGet("Zafir.UI.Next").Latest(true).ForceFoundVersion().Reference()
             ])
         .Embed(
             [
@@ -30,7 +30,7 @@ let test =
         .SourcesFromProject()
         .References(fun r ->
             [
-                r.NuGet("Zafir.UI.Next").Reference()
+                r.NuGet("Zafir.UI.Next").Latest(true).ForceFoundVersion().Reference()
                 r.Project(main)
             ])
 
