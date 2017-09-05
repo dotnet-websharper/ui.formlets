bb45fc517f9e5f76f8b093986f063bdd27ee8338
Jand42 <andras.janko@gmail.com>
2017-09-05 21:51:50 +0200

    Rename Zafir packages to WebSharper

--- build-zafir.fsx
+++ build-zafir.fsx
@@ -3,18 +3,18 @@
 open IntelliFactory.Build
 
 let bt =
-    BuildTool().PackageId("Zafir.UI.Next.Formlets")
-        .VersionFrom("Zafir", "alpha")
+    BuildTool().PackageId("WebSharper.UI.Next.Formlets")
+        .VersionFrom("WebSharper", "alpha")
         .WithFSharpVersion(FSharpVersion.FSharp30)
         .WithFramework(fun fw -> fw.Net40)
 
 let main =
-    bt.Zafir.Library("WebSharper.UI.Next.Formlets")
+    bt.WebSharper4.Library("WebSharper.UI.Next.Formlets")
         .SourcesFromProject()
         .WithSourceMap()
         .References(fun r ->
             [
-                r.NuGet("Zafir.UI.Next").Latest(true).ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.UI.Next").Latest(true).ForceFoundVersion().Reference()
             ])
         .Embed(
             [
@@ -27,12 +27,12 @@ let main =
             ])
 
 let test =
-    bt.Zafir.SiteletWebsite("WebSharper.UI.Next.Formlets.Tests")
+    bt.WebSharper4.SiteletWebsite("WebSharper.UI.Next.Formlets.Tests")
         .SourcesFromProject()
         .WithSourceMap()
         .References(fun r ->
             [
-                r.NuGet("Zafir.UI.Next").Latest(true).ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.UI.Next").Latest(true).ForceFoundVersion().Reference()
                 r.Project(main)
             ])
 
@@ -42,13 +42,13 @@ bt.Solution [
     test
 
     bt.NuGet.CreatePackage()
-        .Description("Provides a framework to build reactive forms in Zafir.")
+        .Description("Provides a framework to build reactive forms in WebSharper.")
         .ProjectUrl("http://github.com/intellifactory/websharper.ui.next.formlets")
         .Configure(fun c ->
             {
                 c with
                     Authors = ["IntelliFactory"]
-                    Title = Some "Zafir.UI.Next.Formlets"
+                    Title = Some "WebSharper.UI.Next.Formlets"
                     LicenseUrl = Some "http://github.com/intellifactory/websharper.ui.next.formlets/blob/master/LICENSE.md"
                     RequiresLicenseAcceptance = true
             })
--- build.fsx
+++ build.fsx
@@ -13,7 +13,7 @@ let main =
         .SourcesFromProject()
         .References(fun r ->
             [
-                r.NuGet("WebSharper.UI.Next").ForceFoundVersion().Reference()
+                r.NuGet("WebSharper.UI.Next").Version("(,4.0)").ForceFoundVersion().Reference()
             ])
         .Embed(
             [
@@ -30,7 +30,7 @@ let test =
         .SourcesFromProject()
         .References(fun r ->
             [
-                r.NuGet("WebSharper.UI.Next").Reference()
+                r.NuGet("WebSharper.UI.Next").Version("(,4.0)").Reference()
                 r.Project(main)
             ])
 
